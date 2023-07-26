1013e7c480dcda00ac722244c5d375443a6a1aa1
davudxD <carovacdavud6@gmail.com>
2023-07-26 15:00:10 +0200

    drugi slucaj

--- Hoisting/script.js
+++ Hoisting/script.js
@@ -1,3 +1,5 @@
+// 1. Slucaj
+
 // const sumConst = (a, b) => a + b
 
 
@@ -10,3 +12,22 @@
 // }
 
 // U ovom slucaju sve radi kao kad je sumFunc bila iznad console logova i konzole nece da vrate error.
+
+
+
+// 2. Slucaj
+
+
+
+console.log(`sumFunc(1, 2) => ${sumFunc(1, 2)}`);
+console.log(`sumConst(3, 4) => ${sumConst(3, 4)}`);
+
+
+const sumConst = (a, b) => a + b
+
+function sumFunc(a, b) {
+    return a+b
+}
+
+// U ovom slucaju sumFunc ce da se ispise u konzoli, a sumConst ce da javi error zato sto je pozvana pre 
+// definisanja.
