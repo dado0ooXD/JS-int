232fb680acade377e841c2b4ec035ca861c0122c
davudxD <carovacdavud6@gmail.com>
2023-07-26 14:57:13 +0200

    prvi slucaj

---
+++ Hoisting/script.js
@@ -0,0 +1,12 @@
+// const sumConst = (a, b) => a + b
+
+
+// console.log(`sumFunc(1, 2) => ${sumFunc(1, 2)}`);
+// console.log(`sumConst(3, 4) => ${sumConst(3, 4)}`);
+
+
+// function sumFunc(a, b) {
+//     return a+b
+// }
+
+// U ovom slucaju sve radi kao kad je sumFunc bila iznad console logova i konzole nece da vrate error.
