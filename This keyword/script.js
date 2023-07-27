660f79fdfddaa8e4386eae028a6e08caaf15fa11
davudxD <carovacdavud6@gmail.com>
2023-07-27 12:02:37 +0200

    drugi nacin

--- This keyword/script.js
+++ This keyword/script.js
@@ -1,5 +1,37 @@
-function Hellow() {
-    console.log(this)
-}
+// method => obj
+// function => global (window, global)
 
-Hellow()
+// Ovaj primer nam vraca window objekat u konzoli.
+
+// function Hellow() {
+//     console.log(this)
+// }
+
+// Hellow()
+
+
+// ======================================================================= //
+
+// const video = {
+//   title: "a",
+//   tags: ["a", "b", "c"],
+//   showTags() {
+//     this.tags.forEach((item) => {
+//       console.log(this.title, item);
+//     });
+//   },
+// };
+
+// video.showTags();
+
+// Ovaj nacin vraca u konzoli:
+// a, a
+// a, b
+// a, c
+
+
+
+// ======================================================================== //
+
+
+
