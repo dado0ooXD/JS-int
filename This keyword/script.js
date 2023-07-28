fb4ec31e74067bc6e2e1c6610d8632d6025503a8
davudxD <carovacdavud6@gmail.com>
2023-07-28 14:45:49 +0200

    this inside a method in class

--- This keyword/script.js
+++ This keyword/script.js
@@ -75,16 +75,46 @@ class Player {
   aboutMe(club) {
     return console.log(`My name is ${this.name}, im ${this.age} years old and i also play the ${this.position} position
         for ${club} football club.`);
-  }
+    }
+    // U ovom slucaju getItem() vraca undefined 3 puta zato sto se console log nalazi u funkciji koja je definisana sa
+    // function keywordom.
+
+    // getItem() {
+    //     [1, 2, 3].map(function (item) { 
+    //         console.log(this)
+    //     })
+    // }
+      
+    // U ovom slucaju getItem() ne vraca undefined vec vraca klasu 3 puta zato sto se console log nalazi u arrow funkciji i
+    // ovo je ispravan nacin.
+    
+      getItem() {
+        [1, 2, 3].map( (item) => { 
+            console.log(this)
+        })
+    }
+    
 }
 
+// Pozivanje getItem() metode:
+const output = new Player("Davud", 18, "190cm", "cam");;
+output.getItem();
+
+// Kreiranje playera:
 const rudiger = new Player("Rudiger", 30, "190cm", "cb");
 const alaba = new Player("Alaba", 29, "182cm", "cb");
 
 rudiger.whatIsThis(); // ===> Player {name: 'Rudiger', age: 30, height: '190cm', position: 'cb'}
 alaba.whatIsThis(); // ===> Player {name: 'Alaba', age: 29, height: '182cm', position: 'cb'}
 
+// Prikazivanje informacija o igracu:
 rudiger.aboutMe("Real Madrid"); // ===> My name is Rudiger, im 30 years old and i also play the cb position
 // for Real Madrid football club.
 alaba.aboutMe("Real Madrid"); // ===> My name is Alaba, im 29 years old and i also play the cb position
 // for Real Madrid football club.
+
+
+
+
+
+
