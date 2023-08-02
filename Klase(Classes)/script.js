5026dea1898d2b383db54ee6b5a44573253ce1b6
davudxD <carovacdavud6@gmail.com>
2023-08-02 23:29:40 +0200

    drugi primer

--- Klase(Classes)/script.js
+++ Klase(Classes)/script.js
@@ -14,4 +14,16 @@
 
 // Kod ovog primera otvorimo ovu klasu u konozoli videcemo ove propsove sto su obelezeni sa this.
 
+class Player {
+  constructor(player, team, position) {
+    console.log("This is a first player!");
+    this.name = player;
+    this.team = team;
+    this.position = position;
+  }
+}
 
+let player1 = new Player("Ronaldo", "Real Madrid", "Striker");
+
+// Ovaj primer vraca isto kao i ovaj gore samo sto smo ovaj put prosledili argumente
+// koji su se trazili u konstruktoru.
