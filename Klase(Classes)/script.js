b8f75c160c96a0a3706c592b4c4e6ceb57cc27a6
davudxD <carovacdavud6@gmail.com>
2023-08-02 23:37:42 +0200

    metode u klasi

--- Klase(Classes)/script.js
+++ Klase(Classes)/script.js
@@ -15,15 +15,24 @@
 // Kod ovog primera otvorimo ovu klasu u konozoli videcemo ove propsove sto su obelezeni sa this.
 
 class Player {
+    // Constructor
+
   constructor(player, team, position) {
     console.log("This is a first player!");
     this.name = player;
     this.team = team;
     this.position = position;
-  }
+    }
+    // Method
+
+    getPlayer(from) {
+        return "First player is " + this.name + " and he's from " + from
+    }
 }
 
 let player1 = new Player("Ronaldo", "Real Madrid", "Striker");
+let player2 = new Player("Messi", "Barcelona", "Striker");
+console.log(player1.getPlayer("Portugal")) // ===> First player is Ronaldo and he's from Portugal.
 
 // Ovaj primer vraca isto kao i ovaj gore samo sto smo ovaj put prosledili argumente
 // koji su se trazili u konstruktoru.
