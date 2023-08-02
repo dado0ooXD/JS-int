2565b05899cdf3bbf2e728491102014152758fe5
davudxD <carovacdavud6@gmail.com>
2023-08-02 23:25:30 +0200

    prvi primer

--- Klase(Classes)/script.js
+++ Klase(Classes)/script.js
@@ -1,10 +1,17 @@
 // Classes
 
-class Player {
-    constructor() {
-        console.log("This is a first player!")
-    }
-}
+// class Player {
+//     constructor() {
+//         console.log("This is a first player!");
+//         console.log(this);
+//         this.name = "Ronaldo"
+//         this.team = "Real Madrid"
+//         this.position = "Striker"
+//     }
+// }
 
-let player1 = new Player();
-let player2 = new Player();
+// let player1 = new Player();
+
+// Kod ovog primera otvorimo ovu klasu u konozoli videcemo ove propsove sto su obelezeni sa this.
+
+
