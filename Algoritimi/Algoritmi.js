6206f0a40b3a897e4cd84846c29056d68878060e
davudxD <carovacdavud6@gmail.com>
2023-10-20 10:18:20 +0200

    get middle items of string

--- Algoritimi/Algoritmi.js
+++ Algoritimi/Algoritmi.js
@@ -20,7 +20,7 @@ function revString(str) {
 
 let a = "Pet";
 let b = "Deset";
-console.log(a, b);
+// console.log(a, b);
 [a, b] = [b, a];
 
 // console.log(a);
@@ -56,4 +56,111 @@ function minMax(arr) {
   return [min, max];
 }
 
-console.log(minMax([12, 43, 2, 54]));
+// console.log(minMax([12, 43, 2, 54]));
+
+// function(2,5) === 5 function(2)(5) === 5
+
+function sum(x) {
+  if (arguments.length === 2) {
+    return arguments[0] + arguments[1];
+  } else {
+    return function (y) {
+      return x + y;
+    };
+  }
+}
+
+// console.log(sum(2, 4));
+// console.log(sum(2)(4));
+
+// arguments vs parameters
+
+const f = function (a, b, r, g, e) {
+  if (f.length === arguments.length) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+};
+
+// f(21, 32, 43); // False
+// f(21, 32, 43, 93, 53); // True
+// f(21); // False
+// f(21, 32, 43, 93, 53, 239); // False
+
+// Promise example
+
+const isEntrenceFree = async (age) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age <= 12) {
+        resolve("Entrance is free.");
+      } else {
+        reject("Sorry, entrance is not free.");
+      }
+    }, 2000);
+  });
+};
+
+// isEntrenceFree(15)
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+// Ne znam ni ja
+
+function mutation(arr) {
+  let firstItem = arr[0].toLowerCase();
+  let secondItem = arr[1].toLowerCase();
+
+  for (var i = 0; i <= secondItem.length; i++) {
+    if (firstItem.indexOf(secondItem[i]) === -1) {
+      return false;
+    }
+    return true;
+  }
+}
+// console.log(mutation(["Alabama", "Boma!"]));
+
+// Is string palindrome
+
+const isPalindrome = (str) => str === str.split("").reverse().join("");
+// console.log(isPalindrome("anavolimilovana"));
+
+// String exist in another
+
+function strInAnother(str, word) {
+  let splitted = str.split(" ");
+
+  // return str.toLowerCase().includes(word.toLowerCase());
+  for (let i = 0; i <= splitted.length; i++) {
+    if (splitted[i].toLowerCase() === word.toLowerCase()) {
+      return true;
+    }
+    return false;
+  }
+}
+
+// console.log(strInAnother("Davud ima 19 godina i 65 kila.", "davud"));
+
+function foo() {
+  let x = (y = 0);
+  x++;
+  y++;
+  return x;
+}
+
+// console.log(foo(), typeof x, typeof y);
+
+// Get middle items of string.
+
+const getMiddle = (str) => {
+  let middle = str.length / 2;
+  if (str.length % 2 === 0) {
+    return str[middle - 1] + str[middle];
+  } else {
+    let middle2 = str.length - 1;
+    return str[middle2 / 2];
+  }
+};
+
+console.log(getMiddle("aw"));
