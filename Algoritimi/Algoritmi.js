32873c917aa8803f8d9801e82e7a02b30ab7fc9e
davudxD <carovacdavud6@gmail.com>
2023-11-09 11:23:38 +0100

    currying example

--- Algoritimi/Algoritmi.js
+++ Algoritimi/Algoritmi.js
@@ -315,3 +315,15 @@ function fizzBuzz(num) {
 }
 
 // fizzBuzz(100);
+
+// Currying
+
+function currying1(a) {
+  return function currying2(b) {
+    return function currying3(c) {
+      return a + b + c;
+    };
+  };
+}
+
+// console.log(currying1(1)(2)(3)); // output: 6
